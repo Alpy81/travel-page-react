8b3b81e91b6e4b29bb8392783593cd2c9e50c84a
Alpy81 <alperen.ertas@gmx.de>
2024-02-11 14:14:31 +0100

    update commit

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-});
+  base: "/github.com/Alpy81/",
+  ...(command !== "serve" && { base: "/travel-page-react/" }),
+}));
